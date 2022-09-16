072b7b5cf1ccfca8bf5b5ec93f0b709c1737c9da
Mishchenko Volodymyr <usonictw1979@gmail.com>
2022-09-16 09:07:58 +0300

    first Commit. Implemented MyList<T extend Number> and implemented Tests

--- src/main/java/org/example/MyList.java
+++ src/main/java/org/example/MyList.java
@@ -1,29 +1,165 @@
 package org.example;
 
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
-public class MyList {
-  public void add(Object o) {
-    throw new RuntimeException("Not implemented");
-  }
+public class MyList<T extends Number> implements Iterable {
 
-  public Object get(int index) {
-    throw new RuntimeException("Not implemented");
-  }
+    private static final int DEFAULT_CAPACITY = 10;
 
-  private void resize() {
-    throw new RuntimeException("Not implemented");
-  }
+    private int size = 0;
 
-  public Object remove(int index) {
-    throw new RuntimeException("Not implemented");
-  }
+    private Object[] array;
 
-  public MyList map(Function f) {
-    throw new RuntimeException("Not implemented");
-  }
+    public MyList() {
+        this.array = new Object[DEFAULT_CAPACITY];
+    }
 
-  public int size() {
-    throw new RuntimeException("Not implemented");
-  }
+    public void add(T o) {
+        resize();
+        array[size] = o;
+        size++;
+//    throw new RuntimeException("Not implemented");
+    }
+
+    public T get(int index) {
+        return (T) array[index];
+
+        //   throw new RuntimeException("Not implemented");
+    }
+
+    private void resize() {
+        int oldCapacity = array.length;
+        if (size == array.length) {
+            int newCapacity = oldCapacity + oldCapacity / 2;
+            array = Arrays.copyOf(array, newCapacity);
+        }
+//    throw new RuntimeException("Not implemented");
+    }
+
+    public T remove(int index) {
+        T removedElem = null;
+        if (index < size && index > 0) {
+            if (index == size - 1) {
+                removedElem = (T) array[index];
+                array[index] = null;
+                size--;
+                return removedElem;
+            } else {
+                removedElem = (T) array[index];
+                for (int i = index; i < size; i++) {
+                    array[i] = array[i + 1];
+                }
+                size--;
+                return removedElem;
+            }
+        } else {
+            throw new NullPointerException();
+        }
+//    throw new RuntimeException("Not implemented");
+    }
+
+    public <R extends Number> MyList<R> map(Function<T, R > f) {
+        MyList<R> list = new MyList<>();
+
+        for (int i = 0; i < size; i++) {
+            list.add(f.apply((T)this.array[i]));
+        }
+        return list;
+
+//        throw new RuntimeException("Not implemented");
+    }
+
+    public int size() {
+        return size;
+//    throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new MyListIterator();
+    }
+
+    /**
+     * <h1>Статический внутренний класс</h1>
+     * <p>Может получить доступ к статическим полям и методам внешнего класса непосредственно.
+     * Но, чтобы получить доступ к полям и методам экземпляра внешнего класса необходимо создать
+     * экземпляр внешнего класса.
+     * <p>
+     * <p><h1>Нестатический внутренний класс</h1>
+     * Может получить доступ к полям и методам своего внешнего класса (оба экземпляра и статическому)
+     * непосредственно без конкретизации (без созания экземпляра).
+     */
+
+
+    private class MyListIterator implements Iterator {
+        private int index = 0;
+        T currentElem = get(index);
+
+        @Override
+        public boolean hasNext() {
+            return currentElem != null;
+        }
+
+        @Override
+        public T next() {
+            if (currentElem == null) {
+                throw new NoSuchElementException();
+            }
+            if (size > 0) {
+                index++;
+                return get(index - 1);
+            }
+            return null;
+        }
+
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        String prefix = "";
+        for (int i = 0; i < size; i++) {
+            sb.append(prefix);
+            prefix = ", ";
+            sb.append(get(i));
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public boolean equals(MyList<? extends Number> list) {
+
+        if (list == this) {
+            return true;
+        }
+
+        if (list == null || list.getClass() != this.getClass()) {
+            return false;
+        }
+
+        boolean equal = true;
+        if (list.size() == this.size()){
+            Object[] elem = array;
+            Object[]elem1 = list.array;
+            for (int i = 0; i < size; i++) {
+                if (!Objects.equals(elem[i], elem1[i])) {
+                    equal = false;
+                    break;
+                }
+            }
+        }
+        return equal;
+    }
+
+    public int hashCode(){
+        int result = 1;
+
+        for (int i = 0; i < size; i++) {
+            result = 31 * result + (array[i] == null ? 0 : array[i].hashCode());
+        }
+        return result;
+
+    }
 }
--- src/test/java/org/example/MyListTest.java
+++ src/test/java/org/example/MyListTest.java
@@ -2,10 +2,192 @@ package org.example;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyListTest {
-  @Test
-  public void test() {
-    Assertions.assertTrue(true);
-  }
+
+
+    @Test
+    public void testAdd() {
+
+        //GIVEN
+        MyList<Number> myList = new MyList<>();
+        int expected = 3;
+
+
+        //WHEN
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+
+        //THEN
+        Assertions.assertEquals(expected, myList.size());
+        Assertions.assertEquals(1, myList.get(0));
+        Assertions.assertEquals(2, myList.get(1));
+        Assertions.assertEquals(3, myList.get(2));
+    }
+
+    @Test
+    public void testAddWithResize() {
+
+        //GIVEN
+        MyList<Number> myList = new MyList<>();
+        int expected = 95;
+
+        //WHEN
+        for (int i = 0; i < 95; i++) {
+            myList.add(i);
+        }
+        //THEN
+        Assertions.assertEquals(expected, myList.size());
+    }
+
+    @Test
+    public void testGet() {
+        //GIVEN
+        MyList<Number> myList = new MyList();
+        int index = 0;
+        int index2 = 1;
+        Integer execute = 2;
+
+        //WHEN
+        myList.add(execute);
+
+        //THEN
+        Assertions.assertEquals(execute, myList.get(index));
+        Assertions.assertNull(myList.get(index2));
+    }
+
+    @Test
+    public void testRemove() {
+        //GIVEN
+        MyList<Number> myList = new MyList();
+        int index = 4;
+        int sizeExecute = 3;
+        int index2 = 1;
+        int sizeExecute2 = 3;
+
+        //WHEN
+        for (int i = 0; i < 5; i++) {
+            myList.add(i);
+        }
+        myList.remove(index);
+        myList.remove(index2);
+
+
+        //THEN
+        Assertions.assertEquals(sizeExecute, myList.size());
+        Assertions.assertEquals(sizeExecute2, myList.size());
+        Assertions.assertNotNull(myList.get(index2));
+        Assertions.assertNull(myList.get(index));
+    }
+
+
+    @Test
+    public void testToString() {
+        //GIVEN
+        MyList<Number> myList = new MyList();
+        String s = "[0, 1, 2, 3, 4]";
+        String s1 = "[0, 1, 2, 3, 4, 5]";
+
+        //WHEN
+        for (int i = 0; i < 5; i++) {
+            myList.add(i);
+        }
+        //THEN
+        Assertions.assertEquals(s, myList.toString());
+        Assertions.assertNotEquals(s1, myList.toString());
+    }
+
+    @Test
+    public void testHashCode(){
+        //GIVEN
+        MyList<Number> myList = new MyList();
+        MyList<Number> myList1 = new MyList();
+
+        //WHEN
+        for (int i = 0; i < 5; i++) {
+            myList.add(i);
+            myList1.add(i);
+        }
+
+        //THEN
+        Assertions.assertEquals(myList.hashCode(), myList1.hashCode());
+        myList1.remove(4);
+        myList1.add(5);
+        Assertions.assertNotEquals(myList1, myList);
+
+    }
+
+    @Test
+    public void testEqualsNull(){
+
+        //GIVEN
+        MyList<Number> myList = new MyList<>();
+        List<Number> list = new ArrayList<>();
+
+        boolean checkExecute = false;
+
+        //WHEN
+        Assertions.assertEquals(checkExecute, myList.equals(null));
+        Assertions.assertEquals(checkExecute, myList.equals(list));
+    }
+
+    @Test
+    public void testEquals(){
+        //GIVEN
+        MyList<Number> myList = new MyList();
+        MyList<Number> myList1 = new MyList();
+        boolean execute = true;
+
+        //WHEN
+        for (int i = 0; i < 5; i++) {
+            myList.add(i);
+            myList1.add(i);
+        }
+
+        //THEN
+        Assertions.assertEquals(execute, myList.equals(myList1));
+        myList1.remove(4);
+        myList1.add(5);
+        Assertions.assertNotEquals(execute, myList.equals(myList1));
+    }
+
+    @Test
+    public void testMap(){
+
+        //GIVEN
+        MyList<Integer> myListInt = new MyList<>();
+        for (int i = 100; i < 500; i += 100 ){
+            myListInt.add(i);
+        }
+        int executeSize = 4;
+        Double dExecute = 100.0;
+        Float fExecute = 200f;
+        Long lExecute = 400L;
+
+        //WHEN
+        MyList<Double> doubleMyList = myListInt.map(Integer::doubleValue);
+        MyList<Float> floatMyList = myListInt.map(Integer::floatValue);
+        MyList<Long> longMyList = myListInt.map(Integer::longValue);
+        MyList<Integer> integerMyList = doubleMyList.map(Double::intValue);
+
+
+        //THEN
+        Assertions.assertEquals(executeSize, doubleMyList.size());
+        Assertions.assertEquals(executeSize, floatMyList.size());
+        Assertions.assertEquals(executeSize, longMyList.size());
+        Assertions.assertEquals(executeSize, integerMyList.size());
+
+        Assertions.assertEquals(dExecute, doubleMyList.get(0));
+        Assertions.assertEquals(fExecute, floatMyList.get(1));
+        Assertions.assertEquals(lExecute, longMyList.get(3));
+        Assertions.assertEquals(100, integerMyList.get(0));
+
+    }
+
+
+
+
 }
